852f352d41215e4932e26a4017ff5f0d39fffc46
Charles <charles.lamb.dev@gmail.com>
2023-12-28 00:36:16 -0500

    added a route to attempt to serve the files although this seems silly to me

--- server.js
+++ server.js
@@ -4,8 +4,15 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000; // Change this to the desired port
 
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/client/public/index.html')); // Replace 'path_to_your_index_html_file' with the actual path to your HTML file
+  });
+
+
+
 // Serve static files from the client directory
-app.use(express.static(path.join(__dirname, '/client/public/index.html')));
+// app.use(express.static(path.join(__dirname, '/client/public/index.html')));
 
 // Start the server
 app.listen(port, () => {
