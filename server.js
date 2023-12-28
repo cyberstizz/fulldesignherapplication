38114802650d35d42e2a8a2dd63cc95e15565e73
Charles <charles.lamb.dev@gmail.com>
2023-12-28 00:25:20 -0500

    added the process.env.port for heroku to use

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 
 const app = express();
-const port = 3000; // Change this to the desired port
+const port = process.env.PORT || 3000; // Change this to the desired port
 
 // Serve static files from the client directory
 app.use(express.static(path.join(__dirname, 'client')));
