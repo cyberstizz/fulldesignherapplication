4d3b514dba67f5b17a4ef65ee700103b720b233a
Charles <charles.lamb.dev@gmail.com>
2023-12-29 18:57:04 -0500

    aded the starteer code for the privaacy policy page

---
+++ client/src/components/PrivacyPolicy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './PrivacyPolicy.scss';
+
+
+
+
+
+const  PrivacyPolicy = () => {
+  return (
+    <div>this is the privacy policy page</div>
+  );
+}
+
+export default PrivacyPolicy;
