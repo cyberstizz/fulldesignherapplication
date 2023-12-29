7b83a00f93bd7c2e3f61f48469352c14f78d66ae
Charles <charles.lamb.dev@gmail.com>
2023-12-29 18:58:39 -0500

    added the starter code for the produc page

---
+++ client/src/components/ProductPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ProductPage.scss';
+
+
+
+
+
+const  ProductPage = () => {
+  return (
+    <div>this is the Privacy policy page</div>
+  );
+}
+
+export default PrroductPage;
