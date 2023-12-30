25123819dcaeda79b6801511b0acca98c4fff2b0
Charles <charles.lamb.dev@gmail.com>
2023-12-29 19:14:51 -0500

    added the starter code for the sneakers page

---
+++ client/src/components/Sneakers.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Sneakers.scss';
+
+
+
+
+
+const  Sneakers = () => {
+  return (
+    <div>this is the Sneakers page</div>
+  );
+}
+
+export default Sneakers;
