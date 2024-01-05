53b48938e2bd4bcf5f3e544d870bee3d5adb1b7d
Charles <charles.lamb.dev@gmail.com>
2024-01-04 22:51:27 -0500

    added earlier underveloped call to the server

--- client/src/components/Crocs.js
+++ client/src/components/Crocs.js
@@ -4,9 +4,57 @@ import './Crocs.scss';
 
 
 const  Crocs = () => {
-  return (
-    <div>this is the Crocs page</div>
-  );
+    const [imagesLoaded, setImagesLoaded] = useState(false);
+    const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+    const [allCrocs, setAllCrocs] = useState([]);
+
+
+    const apiUrl = process.env.NODE_ENV === 'production'
+    ? 'https://designhercustomekreations-c288e9799350.herokuapp.com/'
+    : 'http://localhost:4000';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Axios.get(`${apiUrl}/allCrocs`);
+                setAllCrocs(response.data);
+            } catch (error) {
+                console.error("Error fetching crocs data: ", error);
+            }
+        };
+  
+        fetchData();
+    }, []);
+
+
+
+    const handleImageLoaded = () => {
+        setLoadedImagesCount(prevCount => prevCount + 1);
+};
+
+useEffect(() => {
+  if (loadedImagesCount === AllCrocs.length) {
+    setImagesLoaded(true);
+  }
+}, [loadedImagesCount]);
+
+    return(
+        <React.Fragment>
+            {/* {!imagesLoaded && <Loader />} */}
+
+    
+            <main className='submenuBody'>
+                  {AllCrocs.map(croc => (
+                    <Link key={croc._id} to={`/products/${dress.category}/${dress._id}`}>
+                        <SubMenuComponent onImageLoad={handleImageLoaded} name={dress.title} path={dress.imagePath} />
+                    </Link>
+                ))}
+           
+            
+            </main>
+           
+        </React.Fragment>
+    );
 }
 
 export default Crocs;
