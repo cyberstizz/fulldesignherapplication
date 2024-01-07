7696d8c7160790656113fb3ec029d70061f4dffc
Charles <charles.lamb.dev@gmail.com>
2024-01-06 23:43:12 -0500

    added all of the starter code to fetch all products for each individual page

--- client/src/components/Jackets.js
+++ client/src/components/Jackets.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import './Jackets.scss';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import SubMenuComponent from './SubmenuComponent';
 
 
 
 
+const Jackets = () => {
+    // const [imagesLoaded, setImagesLoaded] = useState(false);
+    // const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+    const [allJackets, setAllJackets] = useState([]);
 
-const  Jackets = () => {
-  return (
-    <div>this is the Jackets page</div>
-  );
+    const baseUrl = window.location.origin;
+
+    const apiUrl = process.env.NODE_ENV === 'production'
+    ? `${baseUrl}`
+    : 'http://localhost:3001';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Axios.get(`${apiUrl}/allJackets`);
+                console.log(response.data.jackets)
+                setAllJackets(response.data.jackets);
+            } catch (error) {
+                console.error("Error fetching jackets data: ", error);
+            }
+        };
+  
+        fetchData();
+    }, []);
+
+
+
+    const handleImageLoaded = () => {
+        // setLoadedImagesCount(prevCount => prevCount + 1);
+};
+
+// useEffect(() => {
+//   if (loadedImagesCount === allCrocs.length) {
+//     setImagesLoaded(true);
+//   }
+// }, [loadedImagesCount]);
+
+    return(
+        <React.Fragment>
+            {/* {!imagesLoaded && <Loader />} */}
+
+    
+            <div className='submenuBody'>
+                  {allJackets.map(Jacket => (
+                    <Link key={Jacket.product_id} to={`/product/${Jacket.product_type}/${Jacket.product_id}`}>
+                        <SubMenuComponent onImageLoad={handleImageLoaded} name={Jacket.name} path={Jacket.image_path} />
+                    </Link>
+                ))}
+           
+            
+            </div>
+           
+        </React.Fragment>
+    );
 }
 
 export default Jackets;
--- client/src/components/Sneakers.js
+++ client/src/components/Sneakers.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import './Sneakers.scss';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import SubMenuComponent from './SubmenuComponent';
 
 
 
 
+const Sneakers = () => {
+    // const [imagesLoaded, setImagesLoaded] = useState(false);
+    // const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+    const [allSneakers, setAllSneakers] = useState([]);
 
-const  Sneakers = () => {
-  return (
-    <div>this is the Sneakers page</div>
-  );
+    const baseUrl = window.location.origin;
+
+    const apiUrl = process.env.NODE_ENV === 'production'
+    ? `${baseUrl}`
+    : 'http://localhost:3001';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Axios.get(`${apiUrl}/allsneakers`);
+                console.log(response.data.sneakers)
+                setAllSneakers(response.data.sneakers);
+            } catch (error) {
+                console.error("Error fetching jackets data: ", error);
+            }
+        };
+  
+        fetchData();
+    }, []);
+
+
+
+    const handleImageLoaded = () => {
+        // setLoadedImagesCount(prevCount => prevCount + 1);
+};
+
+// useEffect(() => {
+//   if (loadedImagesCount === allCrocs.length) {
+//     setImagesLoaded(true);
+//   }
+// }, [loadedImagesCount]);
+
+    return(
+        <React.Fragment>
+            {/* {!imagesLoaded && <Loader />} */}
+
+    
+            <div className='submenuBody'>
+                  {allSneakers.map(sneaker => (
+                    <Link key={sneaker.product_id} to={`/product/${sneaker.product_type}/${sneaker.product_id}`}>
+                        <SubMenuComponent onImageLoad={handleImageLoaded} name={sneaker.name} path={sneaker.image_path} />
+                    </Link>
+                ))}
+           
+            
+            </div>
+           
+        </React.Fragment>
+    );
 }
 
 export default Sneakers;
