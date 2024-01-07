d56f60b6a9ac22aa57d3938719de2b81317511b1
Charles <charles.lamb.dev@gmail.com>
2024-01-06 23:29:32 -0500

    added the code to fetch all of the products for the boots

--- client/src/components/Boots.js
+++ client/src/components/Boots.js
@@ -1,14 +1,67 @@
 import React from 'react';
 import './Boots.scss';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import Axios from 'axios';
+import SubMenuComponent from './SubmenuComponent';
 
 
 
 
+const Boots = () => {
+    // const [imagesLoaded, setImagesLoaded] = useState(false);
+    // const [loadedImagesCount, setLoadedImagesCount] = useState(0);
+    const [allBoots, setAllBoots] = useState([]);
 
-const  Boots = () => {
-  return (
-    <div>this is the boots page</div>
-  );
+    const baseUrl = window.location.origin;
+
+    const apiUrl = process.env.NODE_ENV === 'production'
+    ? `${baseUrl}`
+    : 'http://localhost:3001';
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await Axios.get(`${apiUrl}/allBoots`);
+                console.log(response.data.crocs)
+                setAllBoots(response.data.crocs);
+            } catch (error) {
+                console.error("Error fetching crocs data: ", error);
+            }
+        };
+  
+        fetchData();
+    }, []);
+
+
+
+    const handleImageLoaded = () => {
+        // setLoadedImagesCount(prevCount => prevCount + 1);
+};
+
+// useEffect(() => {
+//   if (loadedImagesCount === allCrocs.length) {
+//     setImagesLoaded(true);
+//   }
+// }, [loadedImagesCount]);
+
+    return(
+        <React.Fragment>
+            {/* {!imagesLoaded && <Loader />} */}
+
+    
+            <div className='submenuBody'>
+                  {allBoots.map(boot => (
+                    <Link key={boot.product_id} to={`/product/${boot.product_type}/${boot.product_id}`}>
+                        <SubMenuComponent onImageLoad={handleImageLoaded} name={boot.name} path={boot.image_path} />
+                    </Link>
+                ))}
+           
+            
+            </div>
+           
+        </React.Fragment>
+    );
 }
 
 export default Boots;
