c9a9c013152ea2da871fbcdb8392477d12d236e3
Charles <charles.lamb.dev@gmail.com>
2024-01-19 02:29:40 -0500

    more changes

--- client/src/components/Jackets.js
+++ client/src/components/Jackets.js
@@ -57,7 +57,7 @@ const Jackets = () => {
 
             <div className='submenuBody'>
                   {allJackets.map(Jacket => (
-                    <Link key={Jacket.product_id} to={`/jackets/${Jacket.product_id}`}>
+                    <Link key={Jacket.product_id} to={`/jacket/${Jacket.product_id}`}>
                         <SubMenuComponent onImageLoad={handleImageLoaded} name={Jacket.name} path={Jacket.image_path} product_price={Jacket.product_price} />
                     </Link>
                 ))}
