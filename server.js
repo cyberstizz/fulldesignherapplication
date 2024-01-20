ccbe838500b5baaf9708cc699a851311f9d31a7e
Charles <charles.lamb.dev@gmail.com>
2024-01-20 06:46:59 -0500

    more

--- server.js
+++ server.js
@@ -10,7 +10,6 @@ const crocsRouter = require('./routes/crocs/crocsRouter')
 const jacketsRouter = require('./routes/jackets/jacketsRouter')
 const bootsRouter = require('./routes/boots/bootsRouter')
 const sneakersRouter = require('./routes/sneakers/sneakersRouter')
-console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const bodyParser = require('body-parser');
 
@@ -155,6 +154,9 @@ app.get('/order/{$order_number}', (req, res) => {
 
 
 app.post('/charge', async (req, res) => {
+
+  console.log('Stripe Secret Key:', process.env.STRIPE_SECRET_KEY);
+
   try {
     // Retrieve the payment method and create a payment intent
     const intent = await stripe.paymentIntents.create({
