1a10d4e91ca67af741cf9aa017d984632df1ddc8
Charles <charles.lamb.dev@gmail.com>
2024-01-24 11:17:28 -0500

    more changes

--- server.js
+++ server.js
@@ -189,8 +189,8 @@ app.post('/payments', async (req, res) => {
     const paymentIntent = await stripe.paymentIntents.create({
       amount,
       currency: 'usd',
-      description: product,
-    });
+      return_url: 'https://designhercustomekreations-c288e9799350.herokuapp.com/',  // Replace with your actual success URL
+  });
     // Retrieve the payment method and create a payment intent
     // const intent = await stripe.paymentIntents.create({
     //   amount,
