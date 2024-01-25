551018361cf1ebe9992d3893c6e775dff849706d
Charles <charles.lamb.dev@gmail.com>
2024-01-25 09:08:56 -0500

    more changes

--- client/src/components/CheckoutButton.js
+++ client/src/components/CheckoutButton.js
@@ -7,30 +7,28 @@ const CheckoutButton = (props) => {
   const [clientSecret, setClientSecret] = useState('');
 
   const handleBuyNow = async () => {
-      // Get the token from the CardElement
-      try {
-        // Use CardElement directly (no need for stripe.elements.getElement)
-        const { token } = await stripe.createToken();
-    
-        // Make a request to your server to create a PaymentIntent
-        const response = await axios.post('/charge', {
-          amount: props.price * 100,
-          token: token.id, // Pass the token.id to the server
-        });
-  
-  
+    try {
+      // Use CardElement directly (no need for stripe.elements.getElement)
+      const { token } = await stripe.createToken();
+      
+      // Make a request to your server to create a PaymentIntent
+      const response = await axios.post('/charge', {
+        amount: props.price * 100,
+        token: token.id, // Pass the token.id to the server
+      });
+
       const { clientSecret } = response.data;
       setClientSecret(clientSecret);
-  
+
       // Confirm the payment on the client side
       const result = await stripe.confirmCardPayment(clientSecret, {
         payment_method: {
-          card: cardElement,
+          card: token.card.id,
         },
       });
-  
+
       console.log('Payment Intent Status:', result.paymentIntent.status);
-  
+
       if (result.error) {
         console.error(result.error.message);
       } else if (result.paymentIntent.status === 'succeeded') {
