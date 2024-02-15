c52251e9dcb56c155d115246073f99d110670351
Charles <charles.lamb.dev@gmail.com>
2024-02-15 00:15:53 -0500

    removed the props from the cart item component

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -8,10 +8,10 @@ import './CartItem.scss';
 const  CartItem = ({path, name, product_price}) => {
   return (
     <main className="fullCartItemComponent">
-    <img onLoad={props.onImageLoad} src={props.path} width='75%'height='100%' style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
+    <img src={path} width='75%'height='100%' style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
     <div className="cartRightSection" syle={{display: 'flex', flexDirection: 'column'}}>
-    <section className="cartTitle">{props.name}</section>
-    <section className="cartPrice">${props.product_price}</section>
+    <section className="cartTitle">{name}</section>
+    <section className="cartPrice">${product_price}</section>
     </div>
     <img src="/cart-icon-28356.png" width='15%'height='30%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
 </main>
