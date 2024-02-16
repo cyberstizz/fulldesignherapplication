05f6db403768b5455d061a8e90780aec7a70995a
Charles <charles.lamb.dev@gmail.com>
2024-02-16 01:59:07 -0500

    more

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -31,7 +31,7 @@ const  CartItem = ({id, path, name, product_price}) => {
     <img src={path} width='75%'height='100%' style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
     <div className="cartRightSection" syle={{display: 'flex', flexDirection: 'column'}}>
     <section className="cartTitle">{name}</section>
-    <button className="deleteButton" onClick={() => openRemoveFromCartModal(props)} >Remove</button>
+    <button className="deleteButton" onClick={() => openRemoveFromCartModal()} >Remove</button>
     <section className="cartPrice">${product_price}</section>
     </div>
     <img src="/cart-icon-28356.png" width='15%'height='30%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
