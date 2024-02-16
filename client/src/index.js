7e6f6d65dc1be61a4775a3c4ba4d5e43a2046d73
Charles <charles.lamb.dev@gmail.com>
2024-02-16 17:36:17 -0500

    changd the configuration of the app in the createRoot method to include the persistgate component

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { store } from './app/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './app/store';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 <Provider store={store}>
-  <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
 </Provider>,
 document.getElementById('root')
 );
