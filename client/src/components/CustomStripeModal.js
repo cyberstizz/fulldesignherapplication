74c601a5ba977cbe8c8f019588f1a6e7a3a2f37b
Charles <charles.lamb.dev@gmail.com>
2024-02-23 20:36:52 -0500

    added the proper lettering function to give the correct product name to the success page

--- client/src/components/CustomStripeModal.js
+++ client/src/components/CustomStripeModal.js
@@ -13,6 +13,33 @@ const CustomStripeModal = ({ isOpen, onClose, totalPrice, productName }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const properLettering = (word) => {
+    //this function will parse a string and remove
+    //any _ characters
+
+    //create a new variable that will be returned
+    //this variable will be an array
+    let newString = [];
+
+
+    for(let i = 0; i < word.length; i++){
+
+    
+    //loop through the string, and for every element
+    //check to see if it is an underscore and if it is
+    //push a blank space into the array '', if it is not
+    if(word[i] !== '_'){
+    //push the letter into the array
+        newString.push(word[i]);
+    } else{
+        newString.push(' ');
+    }
+    }
+    return newString.join('');
+    //after getting out of the array join the array
+    //into a string, and return the string
+}
+
 
   const handleSubmit = async (event) => {
     event.preventDefault();
@@ -49,7 +76,7 @@ const CustomStripeModal = ({ isOpen, onClose, totalPrice, productName }) => {
             console.log("Payment successful:", result.paymentIntent);
             onClose(); 
             dispatch(clearCart());
-            navigate('/success', { state: { itemName: productName } }); // Assuming you have the currentProduct name available here
+            navigate('/success', { state: { itemName: properLettering(productName) } }); // Assuming you have the currentProduct name available here
           }
         }
       } else {
