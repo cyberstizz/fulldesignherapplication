d918ae35b5fceb6354073f06d2f47ffa67c8a752
Charles <charles.lamb.dev@gmail.com>
2024-04-04 09:11:21 -0400

    made chages to resize the desktop view on the submenucomponent

--- client/src/components/SubmenuComponent.js
+++ client/src/components/SubmenuComponent.js
@@ -6,12 +6,12 @@ const SubMenuComponent = (props) => {
     return (
         <React.Fragment>
             <main className="fullSubMenuComponent">
-                <img onLoad={props.onImageLoad} src={props.path} width='75%'height='100%' style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
+                <img className="productImage" onLoad={props.onImageLoad} src={props.path} style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
                 <div className="productRightSection" syle={{display: 'flex', flexDirection: 'column'}}>
                 <section className="submenuTitle">{props.name}</section>
                 <section className="productPrice">${props.product_price}</section>
                 </div>
-                <img src="/cart-icon-28356.png" width='15%'height='30%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
+                <img src="/cart-icon-28356.png" width='15%'height='12%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
             </main>
         </React.Fragment>
     )
