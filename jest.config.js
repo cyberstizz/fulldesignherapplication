7f272393fc9f3db69d2d558c4d7c618b1088ad1f
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-08 21:18:44 -0400

    added the appropriate environemnts in config files for jest in the client and the server

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-    testEnvironment: 'jsdom',
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-  };
-  
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/tests/**/*.test.js'],
+};
