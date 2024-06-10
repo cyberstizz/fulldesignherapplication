ab77b44e2e671391f54f0d7ce2be17c401c1161f
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-10 10:37:04 -0400

    uninstalled and reinstalled the files in the package lock json

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,5 +1,10 @@
 module.exports = {
     testEnvironment: 'jsdom',
     setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+    transform: {
+      '^.+\\.[t|j]sx?$': 'babel-jest',
+    },
+    verbose: true,
+    testURL: "http://localhost/",
   };
   
