2009eb67c278e82efda2f048505613b644ccd9d1
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-18 15:54:55 -0400

    aded a change to the jest config file

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
-    testEnvironment: 'jsdom',
-    moduleNameMapper: {
-      '\\.(css|scss)$': 'identity-obj-proxy'
-    },
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest'
-    },
-    testEnvironmentOptions: {
-      url: "http://localhost"
-    },
-    verbose: true
-  };
-  
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.js'],
+  testEnvironment: 'jsdom',
+  moduleNameMapper: {
+    '\\.(css|scss)$': 'identity-obj-proxy',
+    '\\.(jpg|jpeg|png|gif|webp|svg)$': '<rootDir>/__mocks__/fileMock.js', // Add this line
+  },
+  transform: {
+    '^.+\\.(js|jsx)$': 'babel-jest'
+  },
+  testEnvironmentOptions: {
+    url: "http://localhost"
+  },
+  verbose: true
+};
