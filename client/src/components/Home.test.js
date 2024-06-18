496725b2fd36e7cd13bc17af0588589f7ebc6e40
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-18 14:55:36 -0400

    changes to the home component

--- client/src/components/Home.test.js
+++ client/src/components/Home.test.js
@@ -1,4 +1,3 @@
-// src/components/Home.test.js
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { BrowserRouter as Router } from 'react-router-dom';
