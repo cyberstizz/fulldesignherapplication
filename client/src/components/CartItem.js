51c03ca6e1a731932d60eeaee3bf6175eee8ac3e
Charles Lamb <charleslamb@Charless-MBP.lan>
2024-06-27 15:28:21 -0400

    commented the remove cart item component

--- client/src/components/CartItem.js
+++ client/src/components/CartItem.js
@@ -1,20 +1,14 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { removeFromCart } from '../app/features/cart/cartSlice'
-import './CartItem.scss';
-import RemoveFromCartModal from './RemoveFromCartModal';
-import { useNavigate } from 'react-router';
+import React, { useState } from 'react'; // Import React library and useState hook
+import { useDispatch } from 'react-redux'; // Import useDispatch hook from react-redux for dispatching actions
+import { removeFromCart } from '../app/features/cart/cartSlice'; // Import removeFromCart action, adjust path as necessary
+import './CartItem.scss'; // Import the stylesheet for this component
+import RemoveFromCartModal from './RemoveFromCartModal'; // Import RemoveFromCartModal component
+import { useNavigate } from 'react-router'; // Import hook for navigation
 
-
-
-
-
-const  CartItem = ({id, path, name, product_price}) => {
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const dispatch = useDispatch(); 
-
-  const navigate = useNavigate();
+const CartItem = ({ id, path, name, product_price }) => {
+  const [isModalOpen, setIsModalOpen] = useState(false); // State to manage the modal visibility
+  const dispatch = useDispatch(); // Initialize dispatch function for dispatching actions
+  const navigate = useNavigate(); // Initialize navigate function for navigation
 
   // Function to open the modal
   const openRemoveFromCartModal = () => setIsModalOpen(true);
@@ -22,27 +16,43 @@ const  CartItem = ({id, path, name, product_price}) => {
   // Function to close the modal
   const closeRemoveFromCartModal = () => setIsModalOpen(false);
 
-   // Placeholder function for remove item action
-   const handleRemoveItem = () => {
-    dispatch(removeFromCart({ id }));
+  // Function to handle the remove item action
+  const handleRemoveItem = () => {
+    dispatch(removeFromCart({ id })); // Dispatch the removeFromCart action with the item id
     console.log("Item removed");
-    closeRemoveFromCartModal(); 
+    closeRemoveFromCartModal(); // Close the modal after removing the item
   };
 
   return (
     <main className="fullCartItemComponent">
-    <img src={path} width='75%'height='100%' style={{marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
-    <div className="cartRightSection" syle={{display: 'flex', flexDirection: 'column'}}>
-    <section className="cartTitle">{name}</section>
-    <button className="deleteButton" onClick={() => openRemoveFromCartModal()} >Remove</button>
-    <section className="cartPrice">${product_price}</section>
-    </div>
-    <img src="/cart-icon-28356.png" width='15%'height='30%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
-    
-    <RemoveFromCartModal isOpen={isModalOpen} onClose={closeRemoveFromCartModal} onRemove={handleRemoveItem} />
-
-</main>
+      {/* Image of the product */}
+      <img
+        src={path}
+        width='75%'
+        height='100%'
+        style={{ marginLeft: '-2%', borderTopRightRadius: '50px', borderBottomRightRadius: '50px', border: 'solid 1px silver', borderLeft: '0' }}
+        alt='product'
+      />
+      <div className="cartRightSection" style={{ display: 'flex', flexDirection: 'column' }}>
+        {/* Product name */}
+        <section className="cartTitle">{name}</section>
+        {/* Button to remove the item */}
+        <button className="deleteButton" onClick={openRemoveFromCartModal}>Remove</button>
+        {/* Product price */}
+        <section className="cartPrice">${product_price}</section>
+      </div>
+      {/* Cart icon image */}
+      <img
+        src="/cart-icon-28356.png"
+        width='15%'
+        height='30%'
+        style={{ marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden' }}
+        alt='cart icon'
+      />
+      {/* RemoveFromCartModal component */}
+      <RemoveFromCartModal isOpen={isModalOpen} onClose={closeRemoveFromCartModal} onRemove={handleRemoveItem} />
+    </main>
   );
 }
 
-export default CartItem;
+export default CartItem; // Export the CartItem component as the default export
