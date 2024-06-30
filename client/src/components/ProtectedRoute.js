dcb49b33574cb0e8d340f4808dba71bf9b218049
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-29 20:23:37 -0400

    added the comments for the protected route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -2,6 +2,9 @@ import React from 'react';
 import { Navigate } from 'react-router-dom';
 import './ProtectedRoute.scss';
 
+//simple component to check and see if the request has the proper permissions
+//using the react router dom library
+
 const ProtectedRoute = ({ hasAccess, children }) => {
     if (!hasAccess) {
       // Redirect to home where your login form is, or to a specific login page
