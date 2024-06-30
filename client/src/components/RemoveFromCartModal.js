bc85dca23a829cad449ef901dfe530447f5e5c0d
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-29 20:26:06 -0400

    added the comments for the remove dfrom cart moa

--- client/src/components/RemoveFromCartModal.js
+++ client/src/components/RemoveFromCartModal.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import './RemoveFromCartModal.scss'; // Adjust the path as per your project structure
+import './RemoveFromCartModal.scss';
 
 const RemoveFromCartModal = ({ isOpen, onClose, onRemove }) => {
+  // If the modal is not open, return null
   if (!isOpen) return null;
 
   return (
     <div className="removeFromCartModal-overlay" onClick={onClose}>
+      {/* Prevent event bubbling to avoid closing the modal when clicking inside */}
       <div className="removeFromCartModal" onClick={e => e.stopPropagation()}>
         <p>Remove from cart?</p>
         <div className="modal-actions">
+          {/* Button to confirm removal */}
           <button className="yes-action-btn" onClick={onRemove}>Yes</button>
+          {/* Button to cancel and close the modal */}
           <button className="cancel-action-btn" onClick={onClose}>Cancel</button>
         </div>
       </div>
