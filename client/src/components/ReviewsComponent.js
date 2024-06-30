ec7a1c77bd358450e6c130ca53241c7635f9c72f
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-30 15:09:07 -0400

    added the reviews to the review component

--- client/src/components/ReviewsComponent.js
+++ client/src/components/ReviewsComponent.js
@@ -5,21 +5,26 @@ import './ReviewsComponent.scss';
 import axios from 'axios';
 
 const ReviewsComponent = () => {
+  // State to hold the list of reviews
   const [reviews, setReviews] = useState([]);
+  // State to hold the average rating
   const [averageRating, setAverageRating] = useState(null);
+  // Retrieve productType and productId from URL parameters
   const { productType, productId } = useParams();
 
   useEffect(() => {
+    // Function to fetch reviews from the API
     const fetchReviews = async () => {
       try {
-        // Assuming you have the apiUrl variable set correctly for your environment
+        // Set API URL based on the environment
         const apiUrl = process.env.NODE_ENV === 'production'
-    ? 'https://designhercustomekreations-c288e9799350.herokuapp.com'
-    : 'http://localhost:3001';
+          ? 'https://designhercustomekreations-c288e9799350.herokuapp.com'
+          : 'http://localhost:3001';
 
-
+        // Get reviews for the specific product
         const response = await axios.get(`${apiUrl}/reviews/${productType}/${productId}`);
         
+        // Update state with reviews and average rating
         setReviews(response.data.reviews);
         setAverageRating(response.data.averageRating);
       } catch (error) {
@@ -28,13 +33,15 @@ const ReviewsComponent = () => {
     };
 
     fetchReviews();
-    console.log(reviews)
-  }, [productId, productType]);
+    console.log(reviews);
+  }, [productId, productType]); // Dependencies to re-fetch when productId or productType change
 
   return (
     <div className="reviewsComponent">
+      {/* Display average rating or a placeholder if not available */}
       <h2>Average Rating: {averageRating || 'Not rated yet'}</h2>
       {reviews.length > 0 ? (
+        // Map through reviews and render ReviewComponent for each
         reviews.map((review, index) => (
           <ReviewComponent
             key={index}
@@ -45,6 +52,7 @@ const ReviewsComponent = () => {
           />
         ))
       ) : (
+        // Message if no reviews are present
         <p>No reviews yet.</p>
       )}
     </div>
