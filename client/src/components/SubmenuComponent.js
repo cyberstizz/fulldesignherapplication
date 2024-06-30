62e7649c29a378914f1fe88f3f664f0392a77f22
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-30 15:25:14 -0400

    added the comments for the submenu compoennt

--- client/src/components/SubmenuComponent.js
+++ client/src/components/SubmenuComponent.js
@@ -1,20 +1,35 @@
 import React from "react";
 import './SubMenuComponent.scss';
 
-
 const SubMenuComponent = (props) => {
-    return (
-        <React.Fragment>
-            <main className="fullSubMenuComponent">
-                <img className="productImage" onLoad={props.onImageLoad} src={props.path} style={{marginLeft: '-2%', border: 'solid 1px silver', borderLeft: '0'}} alt='pants'></img>
-                <div className="productRightSection" syle={{display: 'flex', flexDirection: 'column'}}>
-                <section className="submenuTitle">{props.name}</section>
-                <section className="productPrice">${props.product_price}</section>
-                </div>
-                <img src="/cart-icon-28356.png" width='15%'height='12%' style={{marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden'}} alt='cart icon'></img>
-            </main>
-        </React.Fragment>
-    )
+  return (
+    <React.Fragment>
+      <main className="fullSubMenuComponent">
+        {/* Product image */}
+        <img
+          className="productImage"
+          onLoad={props.onImageLoad}
+          src={props.path}
+          style={{ marginLeft: '-2%', border: 'solid 1px silver', borderLeft: '0' }}
+          alt='product'
+        />
+        <div className="productRightSection" style={{ display: 'flex', flexDirection: 'column' }}>
+          {/* Product name */}
+          <section className="submenuTitle">{props.name}</section>
+          {/* Product price */}
+          <section className="productPrice">${props.product_price}</section>
+        </div>
+        {/* Cart icon */}
+        <img
+          src="/cart-icon-28356.png"
+          width='15%'
+          height='12%'
+          style={{ marginTop: '30%', marginRight: '4%', objectFit: 'contain', overflow: 'hidden' }}
+          alt='cart icon'
+        />
+      </main>
+    </React.Fragment>
+  );
 }
 
 export default SubMenuComponent;
