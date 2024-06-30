49dda21d5c2be19469f00eb4e85015e7fc5968e4
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-29 20:26:38 -0400

    now added the comments for the reveiew cocmponent

--- client/src/components/ReviewComponent.js
+++ client/src/components/ReviewComponent.js
@@ -4,9 +4,13 @@ import './ReviewComponent.scss';
 const ReviewComponent = ({ headline, rating, review, userName }) => {
   return (
     <div className="reviewComponent">
+      {/* Display the review headline */}
       <h3 className="headline">{headline}</h3>
+      {/* Display the rating */}
       <div className="rating">Rating: {rating}</div>
+      {/* Display the review text */}
       <p className="review">{review}</p>
+      {/* Display the reviewer's username */}
       <p className="userName">- {userName}</p>
     </div>
   );
