a87bd855b23682d9b45a62ee92c6e1d8eb5cc0bf
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-29 20:08:11 -0400

    added the comments for the failure component

--- client/src/components/Failure.js
+++ client/src/components/Failure.js
@@ -1,12 +1,14 @@
+// Failure.js
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+// Component for displaying payment failure message
 const Failure = () => {
   return (
     <div>
       <h1>Payment Unsuccessful!</h1>
       {/* Link to navigate back to the home page */}
-      <Link to="/"><button>back Home</button></Link>
+      <Link to="/"><button>Back Home</button></Link>
     </div>
   );
 };
