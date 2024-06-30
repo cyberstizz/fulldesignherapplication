14d0d8db4cbd6eed8154a98d014fb13772fb6407
Charles Lamb <charleslamb@Charless-MacBook-Pro.local>
2024-06-29 20:17:14 -0400

    added the comments for the logout modal

--- client/src/components/LogoutModal.js
+++ client/src/components/LogoutModal.js
@@ -1,17 +1,18 @@
-// LoginModal.js
 import React from 'react';
 import './LogoutModal.scss';
 
 const LogoutModal = ({ isOpen, onClose }) => {
+  // Do not render the modal if it's not open
   if (!isOpen) return null;
 
+  // Render the modal
   return (
     <div className="modal-overlay" onClick={onClose}>
       <div className="modal" onClick={(e) => e.stopPropagation()}>
         <button className="close-button" onClick={onClose}>
           &times;
         </button>
-        {/* Add your modal content here */}
+        {/* Modal content for logout */}
         <div>Logout Modal</div>
       </div>
     </div>
